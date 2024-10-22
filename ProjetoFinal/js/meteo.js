62cd79eac2ba9a10fe959f1df8e3d98a33aba55b
Vanessa Pereira <vanessappereira@outlook.pt>
2024-10-23 00:24:51 +0100

    updated project

---
+++ ProjetoFinal/js/meteo.js
@@ -0,0 +1,82 @@
+async function getImagemPrecipitacao(precipitaProb) {
+    // Convert into double
+    const doublePrecipitacao = parseFloat(precipitaProb);
+    console.log(doublePrecipitacao);
+
+    if (doublePrecipitacao === 0) {
+        return 'sol.png';
+    } else if (doublePrecipitacao <= 30) {
+        return 'nublado.png';
+    } else if (doublePrecipitacao <= 70) {
+        return 'aguaceiros.png';
+    } else {
+        return 'chuva.png';
+    }
+}
+
+async function fetchMeteorologia(districtId, districtName) {
+    const apiURL = `https://api.ipma.pt/open-data/forecast/meteorology/cities/daily/${districtId}.json`;
+    const response = await fetch(apiURL);
+    const dataForecast = await response.json();
+
+    let meteoHTML = '<table>';
+    for (const item of dataForecast.data) {
+        const imageSrc = await getImagemPrecipitacao(item.precipitaProb);
+
+        meteoHTML += `
+        <tr class="tabela">
+            <td>Date: ${item.forecastDate}</td>
+            <td><img src="../images/${imageSrc}" height="55" alt="Weather Image"></td>
+            <td>Min Temperature: ${item.tMin}°C</td>
+            <td>Max Temperature: ${item.tMax}°C</td>
+        </tr>`;
+    }
+    meteoHTML += '</table>';
+    document.getElementById('previsao').innerHTML += `<h5>${districtName}</h5>${meteoHTML}`;
+}
+
+
+
+
+async function obterPrevisao() {
+    const url = 'https://api.ipma.pt/open-data/distrits-islands.json';
+
+    try {
+        const response = await fetch(url);
+
+        // Check if the response is okay (status in the range 200-299)
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const dataAPI = await response.json();
+        const dataLocalDistrict = dataAPI.data;
+        //console.log(dataLocalDistrict);
+
+        const titleId = document.getElementById("title").textContent;
+        console.log(titleId);
+
+        /* Almeida, Castelo Rodrigo, Linhares da Beira e Marialva pertencem ao distrito da Guarda; 
+        * Monsanto pertence ao distrito de Castelo Branco. */
+        let forecast;
+
+        if (['Almeida', 'Castelo Rodrigo', 'Linhares da Beira', 'Marialva'].includes(titleId)) {
+            forecast = dataLocalDistrict.find(district => district.local === 'Guarda');
+        } else if (titleId === 'Monsanto') {
+            forecast = dataLocalDistrict.find(district => district.local === 'Castelo Branco');
+        }
+
+        if (forecast) {
+            const selectedValue = forecast.local;
+            const idValue = forecast.globalIdLocal;
+            console.log("id value: " + idValue);
+            await fetchMeteorologia(idValue, selectedValue);
+        } else {
+            document.getElementById('previsao').innerHTML += '<p class="text-danger">Local não encontrado.</p>';
+        }
+    } catch (error) {
+        console.log(error);
+        document.getElementById('previsao').innerHTML += '<p class="text-danger">Erro a encontrar meteorologia!</p>';
+    }
+}
+window.onload = obterPrevisao; // Fetch distritos no reload
